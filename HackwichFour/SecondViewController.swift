3af63739d81522f635fe955fa718278b46764bdd
Steven Chapman <steven44@hawaii.edu>
2020-02-11 10:32:04 -1000

    Completed Part 9

---
+++ HackwichFour/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  HackwichFour
+//
+//  Created by CM Student on 2/11/20.
+//  Copyright © 2020 Steven Chapman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var SecondLabelTab: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
